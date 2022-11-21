4f9e995269d0d7ffd962413628074286d66c7c4b
graceywambui <grace.wambui@student.moringaschool.com>
2022-11-21 11:42:41 +0300

    objects lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = {}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return Object.assign({}, employee, { [key]:value});
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key]=value;
+
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newObject = Object.assign({},employee);
+    delete newObject[key];
+    return newObject;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    Object.assign({}, employee);
+    delete employee[key];
+    return employee;
+}
